1aaeadc310529fbe165356039d3f0038addbe401
Alex House <ahouse101@gmail.com>
2021-04-04 20:54:23 -0700

    Added all components to main export, bumped version to match earlier published version.

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,19 @@
+import Modal from './components/Modal.vue';
+import Preloader from './components/Preloader.vue';
+import SplitContainer from './components/SplitContainer.vue';
+import Toolbar from './components/Toolbar.vue';
+import ToolbarButton from './components/ToolbarButton.vue';
+import ToolbarDropdown from './components/ToolbarDropdown.vue';
+
 import Demo from './components/Demo.vue';
 
 export {
+	Modal,
+	Preloader,
+	SplitContainer,
+	Toolbar,
+	ToolbarButton,
+	ToolbarDropdown,
+	
 	Demo
 };
